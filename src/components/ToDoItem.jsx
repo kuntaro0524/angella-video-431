af3997d21269a6c2714b491aff021fcb1c72fde6
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-14 15:20:53 +0900

    Todoリストの中のテキストをクリックしたら線が引かれるという実装 できた！（自分の答え）

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const ToDoItem = (props) => {
   const { todoItems } = props;
+  const [doneFlag, setDoneFlag] = useState([]);
+
+  const onClickButton = (index) => {
+    console.log(index);
+    const currentFlags = [...doneFlag];
+    console.log(currentFlags);
+
+    currentFlags[index] = !currentFlags[index];
+    setDoneFlag(currentFlags);
+  };
+
   return (
     <>
       <ul>
-        {todoItems.map((todoItem) => (
-          <li>{todoItem}</li>
+        {todoItems.map((todoItem, index) => (
+          <li
+            key={todoItem}
+            onClick={() => onClickButton(index)}
+            style={doneFlag[index] ? { textDecoration: "line-through" } : {}}
+          >
+            {todoItem}
+          </li>
         ))}
       </ul>
     </>
